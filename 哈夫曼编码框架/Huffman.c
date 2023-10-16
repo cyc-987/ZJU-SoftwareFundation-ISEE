e2dd524830fc6013812d0705e980ac428a2c80e0
cyc-987 <2516803593@qq.com>
2023-10-16 21:56:25 +0800

    完成哈夫曼

--- 哈夫曼编码框架/Huffman.c
+++ 哈夫曼编码框架/Huffman.c
@@ -11,11 +11,58 @@ typedef struct huffman_node
 
 void SelectIJ(int k, bnode node[], int *i, int *j)
 {
-
+	*i = *j = 0;
+	int min1 = 32767, min2 = 32767;
+	for(int m = 0;m<k;m++){
+		if(node[m].parent == 0){
+			if(node[m].data < min1){
+				min2 = min1;
+				*j = *i;
+				min1 = node[m].data;
+				*i = m;
+			}
+			else if(node[m].data < min2){
+				min2 = node[m].data;
+				*j = m;
+			}
+		}
+	}
 }
 
 void HuffmanTree(int n, bnode node[],int w[])
 {
+	int i,j;
+	for(int i = 0;i<n;i++){
+		node[i].data = w[i];
+		node[i].lchild = node[i].rchild = 0;
+	}
+	for(int i = 0;i<2*n-1;i++) node[i].parent = 0;
+	for(int k = n;k<2*n-1;k++){
+		SelectIJ(k,node,&i,&j);
+		node[k].data = node[i].data + node[j].data;
+		node[k].lchild = i;
+		node[k].rchild = j;
+		node[i].parent = node[j].parent = k;
+	}
+}
+
+void printHuffmanTree(int i, bnode node[])
+{
+	int code[26];
+	int n = 0, j = 0;
+	do
+	{
+		if(node[node[i].parent].lchild == i){
+			code[n++] = 0;
+		}
+		else{
+			code[n++] = 1;
+		}
+		i = node[i].parent;
+	}while(i != 50);
+	for(int j = n-1;j>=0;j--){
+		printf("%d",code[j]);
+	}
 }
 
 void main()
@@ -70,13 +117,13 @@ void main()
 	node = (bnode *)malloc((2*26-1) * sizeof(bnode));
 
 	//½¨Á¢¹þ·òÂüÊ÷
-	//HuffmanTree(26, node, weight);
+	HuffmanTree(26, node, weight);
 
 	//¹þ·òÂü±àÂë
 	for (i=0; i<26; i++)
 	{
 		printf("Now processing %c -----", i+65);
-		
+		printHuffmanTree(i, node);
 		printf("\n");
 	}
 
